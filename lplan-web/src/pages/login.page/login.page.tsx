0f4e08e2d5f47a64f92c5d17c73048380bfb038c
Quim Moran M <marc.moran@estudiantat.upc.edu>
2023-05-07 03:58:06 +0200

    feat: addition of a Back button in both Register and Login pages.

--- lplan-web/src/pages/login.page/login.page.tsx
+++ lplan-web/src/pages/login.page/login.page.tsx
@@ -6,13 +6,17 @@ import './login.page.css';
 
 // Fondo de pantalla personalizado ...
 import backgroundImage from '../../assets/images/background_2.jpg';
+import { Link } from "react-router-dom";
 
 const Login = () => {
   useEffect(() => {
     document.body.style.backgroundImage = `url(${backgroundImage})`;
   }, []);
   return (
-    <div>
+    <div className="register-container">
+      <div className="button-container-back">
+        <Link to="/" className="buttonBack">Back</Link>
+      </div>
       <div className="containerSection">
         <h1 className="titleSection">Login</h1>
       </div>
--- lplan-web/src/pages/register.page/register.page.tsx
+++ lplan-web/src/pages/register.page/register.page.tsx
@@ -6,13 +6,17 @@ import './register.page.css';
 
 // Fondo de pantalla personalizado ...
 import backgroundImage from '../../assets/images/background_1.jpg';
+import { Link } from "react-router-dom";
 
 const Register = () => {
   useEffect(() => {
     document.body.style.backgroundImage = `url(${backgroundImage})`;
   }, []);
   return (
-    <div>
+    <div className="register-container">
+      <div className="button-container-back">
+        <Link to="/" className="buttonBack">Back</Link>
+      </div>
       <div className="containerSection">
         <h1 className="titleSection">Register</h1>
       </div>
