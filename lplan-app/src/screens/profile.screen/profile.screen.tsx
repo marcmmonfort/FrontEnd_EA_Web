cfb69745930fc86666770f202006fc79c5d2f525
VictorFernandezEscriva <Victor.Fernandez.Escriva@estudiantat.upc.edu>
2023-05-09 12:35:49 +0200

    style: customization of the main pages (not finished yet).

--- lplan-app/src/screens/profile.screen/profile.screen.tsx
+++ lplan-app/src/screens/profile.screen/profile.screen.tsx
@@ -1,12 +1,39 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Animated, Image } from 'react-native';
+import * as Font from 'expo-font';
+
+async function loadFonts() {
+  await Font.loadAsync({
+    'SF UI Display': require('../../../assets/fonts/SF-UI-Display-Semibold.ttf'),
+  });
+  await Font.loadAsync({
+    'Rafaella': require('../../../assets/fonts/Rafaella.ttf'),
+  });
+}
+
+loadFonts();
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Rafaella',
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
 
 function ProfileScreen(){
     return (
-        <View>
-          <Text>Profile</Text>
+        <View style={styles.container}>
+          <Text style={styles.text}>Profile</Text>
         </View>
-      );
+    );
 }
 
 export default ProfileScreen;
