b5a8738b3cf6776f86f90cc52d3b878d09a15a67
Genisc13 <genis.castillo@estudiantat.upc.edu>
2023-05-15 14:11:20 +0200

    chore: addapt profile page with css

--- lplan-web/src/pages/profile.page/profile.page.tsx
+++ lplan-web/src/pages/profile.page/profile.page.tsx
@@ -51,8 +51,8 @@ const Profile = () => {
             </div>
             <div className="profile-user-settings">
               <h1 className="profile-user-name">{currentUser.appUser}</h1>
-              <button className="btn profile-edit-btn">Edit Profile</button>
-              <button className="btn profile-settings-btn" aria-label="profile settings"><i className="fas fa-cog" aria-hidden="true"></i></button>
+              <button className="btn_profile-edit-btn">Edit Profile</button>
+              <button className="btn_profile-settings-btn" aria-label="profile settings"> Settings <i className="fas fa-cog" aria-hidden="true"></i></button>
             </div>
             <div className="profile-stats">
               <ul>
@@ -61,7 +61,8 @@ const Profile = () => {
               </ul>
             </div>
             <div className="profile-bio">
-              <p><span className="profile-real-name">{currentUser.nameUser}</span>{currentUser.descriptionUser}</p>
+              <p>Name: <span className="profile-real-name">{currentUser.nameUser}</span></p>
+              <p>Description: {currentUser.descriptionUser}</p>
             </div>
           </div>
         </div>        
