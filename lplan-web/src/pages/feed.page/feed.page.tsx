5984278bdbfe5658c332e7db64f24d4aa8ac34dc
Quim Moran M <marc.moran@estudiantat.upc.edu>
2023-05-23 15:20:36 +0200

    style: edition of the feed style.

--- lplan-web/src/pages/feed.page/feed.page.tsx
+++ lplan-web/src/pages/feed.page/feed.page.tsx
@@ -6,155 +6,179 @@ import { PublicationService } from "../../services/publication.service";
 import { AuthService } from "../../services/auth.service";
 
 // Fondo de pantalla personalizado ...
-import backgroundImage from '../../assets/images/background_3.jpg';
-import './feed.page.css';
+import backgroundImage from "../../assets/images/background_3.jpg";
+import "./feed.page.css";
 import { CommentService } from "../../services/comment.service";
 import { Comment } from "../../models/comment.model";
 import { UserService } from "../../services/user.service";
 import { Link } from "react-router-dom";
 
-
 const Feed = () => {
-
   const [listPublications, setListPublications] = useState<Publication[]>([]);
   const [numPagePublication, setNumPagePublication] = useState<number>(1);
-  const [commentsVisibility, setCommentsVisibility] = useState<{ [key: string]: boolean }>({});
+  const [commentsVisibility, setCommentsVisibility] = useState<{[key: string]: boolean; }>({});
   const [pageComments, setPageComments] = useState<{ [key: string]: number }>({});
   const [commentButton, setCommentButton] = useState<{ [key: string]: string }>({});
-  const [listCommentsPublication, setListCommentsPublication] = useState<{ [key: string]: Comment[] }>({});
+  const [listCommentsPublication, setListCommentsPublication] = useState<{[key: string]: Comment[];}>({});
+  const [datePublication, setDatePublication] = useState<{[key: string]: string;}>({});
 
   useEffect(() => {
     document.body.style.backgroundImage = `url(${backgroundImage})`;
     console.log("Iniciamos");
     const userId = AuthService.getCurrentUser();
-    if(userId){
-      PublicationService.feed(numPagePublication.toString(),userId)
-      .then(response => {
-        console.log(response);
-        console.log(response.data);
+    if (userId) {
+      PublicationService.feed(numPagePublication.toString(), userId)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+
+          const initialVisibility = response.data.reduce(
+            (acc: { [key: string]: boolean }, publication: Publication) => {
+              acc[publication.uuid] = false;
+              return acc;
+            },
+            {}
+          );
+          setCommentsVisibility(initialVisibility);
+
+          const initialPage = response.data.reduce(
+            (acc: { [key: string]: number }, publication: Publication) => {
+              acc[publication.uuid] = 1;
+              return acc;
+            },
+            {}
+          );
+          setPageComments(initialPage);
+
+          const initialCommentButton = response.data.reduce(
+            (acc: { [key: string]: string }, publication: Publication) => {
+              acc[publication.uuid] = "Show comments";
+              return acc;
+            },
+            {}
+          );
+          setCommentButton(initialCommentButton);
+
+          const initialListComments = response.data.reduce(
+            (acc: { [key: string]: Comment[] }, publication: Publication) => {
+              acc[publication.uuid] = [];
+              return acc;
+            },
+            {}
+          );
+          setListCommentsPublication(initialListComments);
         
-        const initialVisibility = response.data.reduce((acc: { [key: string]: boolean }, publication: Publication) => {
-          acc[publication.uuid] = false;
-          return acc;
-        }, {});
-        setCommentsVisibility(initialVisibility);
 
-        const initialPage = response.data.reduce((acc: { [key: string]: number }, publication: Publication) => {
-          acc[publication.uuid] = 1;
-          return acc;
-        }, {});
-        setPageComments(initialPage);
+          setListPublications(response.data);
 
-        const initialCommentButton = response.data.reduce((acc: { [key: string]: string }, publication: Publication) => {
-          acc[publication.uuid] = "Show comments";
-          return acc;
-        }, {});
-        setCommentButton(initialCommentButton);
-
-        const initialListComments = response.data.reduce((acc: { [key: string]: Comment[] }, publication: Publication) => {
-          acc[publication.uuid] = [];
-          return acc;
-        }, {});
-        setListCommentsPublication(initialListComments);
-
-        setListPublications(response.data);
-      })
-      .catch(error => {
-        window.location.href = '*';
-      });
+        })
+        .catch((error) => {
+          window.location.href = "*";
+        });
     }
   }, [numPagePublication]);
 
   const handleLoadMore = () => {
     console.log("Has pulsado el btn");
-    setNumPagePublication(prevPage => prevPage + 1);
+    setNumPagePublication((prevPage) => prevPage + 1);
     const userId = AuthService.getCurrentUser();
     console.log("HandleLoadMore:" + numPagePublication);
-    if(userId){
-      PublicationService.feed((numPagePublication).toString(), userId)
-      .then(response => {
-        console.log(response);
-        console.log(response.data);
-        setListPublications(prevPublications => [...prevPublications, ...response.data]);
-      })
-      .catch(error => {
-        window.location.href = '*';
-      });
+    if (userId) {
+      PublicationService.feed(numPagePublication.toString(), userId)
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          setListPublications((prevPublications) => [
+            ...prevPublications,
+            ...response.data,
+          ]);
+        })
+        .catch((error) => {
+          window.location.href = "*";
+        });
     }
-  }
+  };
 
   const getComments = (idPublication: string) => {
     console.log("Ver comentarios");
     console.log("idPublication: " + idPublication);
-    console.log("commentsVisibility[PublicationId]=" + commentsVisibility[idPublication]);
+    console.log(
+      "commentsVisibility[PublicationId]=" + commentsVisibility[idPublication]
+    );
     console.log("pageComments[PublicationId]=" + pageComments[idPublication]);
-    setCommentsVisibility(prevVisibility => {
+    setCommentsVisibility((prevVisibility) => {
       const updatedVisibility = {
         ...prevVisibility,
-        [idPublication]: !prevVisibility[idPublication]
+        [idPublication]: !prevVisibility[idPublication],
       };
       console.log("second " + updatedVisibility[idPublication]);
 
       if (updatedVisibility[idPublication]) {
-        setCommentButton(prevCommentButton => ({
+        setCommentButton((prevCommentButton) => ({
           ...prevCommentButton,
-          [idPublication]: prevCommentButton[idPublication] = "Hide Comments"
+          [idPublication]: (prevCommentButton[idPublication] = "Hide Comments"),
         }));
         console.log("Entro a hide");
-        CommentService.getCommentsPublication(idPublication, (pageComments[idPublication]).toString())
-        .then(response => {
-          console.log(response);
-          console.log(response.data);
-          setListCommentsPublication(prevListComments => ({
-            ...prevListComments,
-            [idPublication]: response.data
-          }));
-        })
-        .catch(error => {
-          window.location.href = '*';
-        });
+        CommentService.getCommentsPublication(
+          idPublication,
+          pageComments[idPublication].toString()
+        )
+          .then((response) => {
+            console.log(response);
+            console.log(response.data);
+            setListCommentsPublication((prevListComments) => ({
+              ...prevListComments,
+              [idPublication]: response.data,
+            }));
+          })
+          .catch((error) => {
+            window.location.href = "*";
+          });
       } else {
-        setCommentButton(prevCommentButton => ({
+        setCommentButton((prevCommentButton) => ({
           ...prevCommentButton,
-          [idPublication]: prevCommentButton[idPublication] = "Show Comments"
+          [idPublication]: (prevCommentButton[idPublication] = "Show Comments"),
         }));
         console.log("Entro a show");
-        setListCommentsPublication(prevListComments => ({
+        setListCommentsPublication((prevListComments) => ({
           ...prevListComments,
-          [idPublication]: []
+          [idPublication]: [],
         }));
-        setPageComments(prevPageComments => ({
+        setPageComments((prevPageComments) => ({
           ...prevPageComments,
-          [idPublication]: prevPageComments[idPublication] = 1
+          [idPublication]: (prevPageComments[idPublication] = 1),
         }));
       }
-        return updatedVisibility;
+      return updatedVisibility;
     });
     console.log("Page comentarios:" + pageComments[idPublication]);
-  }
+  };
 
   const showMoreComments = (idPublication: string) => {
     console.log("Ver más comentarios");
-    setPageComments(prevPageComments => ({
+    setPageComments((prevPageComments) => ({
       ...prevPageComments,
-      [idPublication]: prevPageComments[idPublication] +1
+      [idPublication]: prevPageComments[idPublication] + 1,
     }));
-    CommentService.getCommentsPublication(idPublication, (pageComments[idPublication] +1).toString())
-      .then(response => {
+    CommentService.getCommentsPublication(
+      idPublication,
+      (pageComments[idPublication] + 1).toString()
+    )
+      .then((response) => {
         console.log(response);
         console.log(response.data);
-        setListCommentsPublication(prevListComments => ({
+        setListCommentsPublication((prevListComments) => ({
           ...prevListComments,
-          [idPublication]: [...prevListComments[idPublication], ...response.data]
+          [idPublication]: [
+            ...prevListComments[idPublication],
+            ...response.data,
+          ],
         }));
       })
-      .catch(error => {
-        window.location.href = '*';
+      .catch((error) => {
+        window.location.href = "*";
       });
-  }
-
-
-
+  };
 
   return (
     <div>
@@ -165,66 +189,122 @@ const Feed = () => {
       <div className="feed">
         {listPublications.map((publication) => (
           <div className="post" key={publication.uuid}>
-
             <div className="post__header">
-              <img className="post__profile-img" src={`${publication.idUser.photoUser}`} alt="Profile"/>
-                <div className="post__info">
-                  <p className="post__username">{publication.idUser.nameUser} {publication.idUser.surnameUser}</p>
-                  <p className="post__timestamp">{(publication.createdAt).toLocaleString()}</p>
-                </div>
+              <img
+                className="post__profile-img"
+                src={`${publication.idUser.photoUser}`}
+                alt="Profile"
+              />
+              <div className="post__info">
+                <p className="post__username_header">
+                  {publication.idUser.appUser}
+                </p>
+                <p className="post__timestamp_header">
+                  {new Date(publication.createdAt).toLocaleString()}
+                </p>
+              </div>
             </div>
 
             <div className="post__body">
+              {publication.photoPublication.map((photo) => (
+                <img
+                  className="post__image"
+                  key={photo}
+                  src={photo}
+                  alt="Post"
+                />
+              ))}
+              
               <p className="post__text">{publication.textPublication}</p>
-              {publication.photoPublication.map((photo) => ( <img className="post__image" key={photo} src={photo} alt="Post"/> ))}
               <div style={{ textAlign: "center" }}>
-                <button className="show__comments" onClick={() => { getComments(publication.uuid.toString()); }}>
-                {commentsVisibility[publication.uuid]} {commentButton[publication.uuid]} {publication.commentsPublication?.length}
+                <button
+                  className="show__comments"
+                  onClick={() => {
+                    getComments(publication.uuid.toString());
+                  }}
+                >
+                  {commentsVisibility[publication.uuid]}{" "}
+                  {commentButton[publication.uuid]}{" "}
+                  {publication.commentsPublication?.length}
                 </button>
               </div>
-              {commentsVisibility[publication.uuid] &&  (
-              <div> {listCommentsPublication[publication.uuid].map((comment) => ( 
-                <div className="commentContainer" key={comment.uuid}>                  
-                  
-                  <Link to={`/user/${comment.idUserComment.uuid}`} className="user-link">
-                    <div className="user">
-                    {comment.idUserComment.photoUser ? (<img src={comment.idUserComment.photoUser} alt={comment.idUserComment.nameUser} className="user__profile-img" />) : (
-                        <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="user__profile-img" />
-                      )}
-                      <div className="user__info">
-                        <p className="user__username">@{comment.idUserComment.appUser}</p>
-                      </div>
+              {commentsVisibility[publication.uuid] && (
+                <div>
+                  {" "}
+                  {listCommentsPublication[publication.uuid].map((comment) => (
+                    <div className="commentContainer_2" key={comment.uuid}>
+                      <Link
+                        to={`/user/${comment.idUserComment.uuid}`}
+                        className="user-link"
+                      >
+                        <div className="user_2">
+                          {comment.idUserComment.photoUser ? (
+                            <img
+                              src={comment.idUserComment.photoUser}
+                              alt={comment.idUserComment.nameUser}
+                              className="user__profile-img_2"
+                            />
+                          ) : (
+                            <img
+                              src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+                              alt="profile-img"
+                              className="user__profile-img_2"
+                            />
+                          )}
+                          <div className="user__info">
+                            <p className="user__commentname">
+                              @{comment.idUserComment.appUser}
+                            </p>
+                            <span className="comment__text">{comment.textComment}</span>
+                          </div>
+                        </div>
+                      </Link>
                     </div>
-                  </Link>
-                  
-                  <span className="commentText">{comment.textComment}</span>
-                  
-                </div> ))}
-                {publication.commentsPublication && publication.commentsPublication.length > (pageComments[publication.uuid] ?? 0) * 2 ? (
-                  <button className="show_more_comments" onClick={() => { showMoreComments(publication.uuid); }}>
-                    {'Show More'}
-                  </button>
+                  ))}
+                  {publication.commentsPublication &&
+                  publication.commentsPublication.length >
+                    (pageComments[publication.uuid] ?? 0) * 2 ? (
+                    <button
+                      className="show_more_comments"
+                      onClick={() => {
+                        showMoreComments(publication.uuid);
+                      }}
+                    >
+                      {"Show More"}
+                    </button>
                   ) : (
-                  <button className="show_more_comments" onClick={() => { showMoreComments(publication.uuid); }} disabled>
-                    {'Show More'}
-                  </button>
-                )}
-
-
-              </div> )}
+                    <button
+                      className="show_more_comments"
+                      onClick={() => {
+                        showMoreComments(publication.uuid);
+                      }}
+                      disabled
+                    >
+                      {"Show More"}
+                    </button>
+                  )}
+                </div>
+              )}
             </div>
-
           </div>
         ))}
         <div className="load-more">
-        {listPublications.length > numPagePublication * 3 ? (
-          <button className="buttonLoadMore" onClick={handleLoadMore}>Load More</button>
+          {listPublications.length > numPagePublication * 3 ? (
+            <button className="buttonLoadMore" onClick={handleLoadMore}>
+              Load More
+            </button>
           ) : (
-            <button className="buttonLoadMore" onClick={handleLoadMore} disabled>Load More </button>
+            <button
+              className="buttonLoadMore"
+              onClick={handleLoadMore}
+              disabled
+            >
+              Load More{" "}
+            </button>
           )}
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
