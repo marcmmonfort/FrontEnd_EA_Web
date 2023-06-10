9bf3d7838c1cce09083757d53de18db81ec1df1c
eloimoncho <eloi.moncho@estudiantat.upc.edu>
2023-06-10 18:54:28 +0200

    feat: add discovery in calendar

--- lplan-web/src/components/activityDetails/activity.component.tsx
+++ lplan-web/src/components/activityDetails/activity.component.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ActivityEntity } from "../../models/activity.model";
 import "./activity.component.css"
+import { UserService } from "../../services/user.service";
+import { User } from "../../models/user.model";
+import { Link } from "react-router-dom";
 
 interface ActivityDetailsModalProps {
   activity: ActivityEntity;
@@ -18,6 +21,23 @@ const ActivityDetailsModal: React.FC<ActivityDetailsModalProps> = ({
     const [participants, setParticipants] = useState(activity.participantsActivity || []);
     const [isCurrentUserParticipant, setIsCurrentUserParticipant] = useState(participants.includes(userId));
     const [isCreatorOfActivity, setIsCreatorOfActivity] = useState(activity.creatorActivity.includes(userId));
+    const [creatorUser, setCreatorUser] = useState<User | null>(null);
+    const [creatorAppName, setCreatorAppName] = useState<string>("");
+
+  useEffect(() => {
+    const fetchCreatorAppName = async (uuid: string) => {
+      try {
+        const response = await UserService.getPerson(uuid);
+        const user = response.data;
+        setCreatorUser(user);
+        setCreatorAppName(user.appUser || "");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchCreatorAppName(activity.creatorActivity);
+  }, [activity.creatorActivity]);
+
     
     const handleAddToActivity = (isJoining: boolean) => {
       setIsCurrentUserParticipant(!isCurrentUserParticipant);
@@ -26,7 +46,6 @@ const ActivityDetailsModal: React.FC<ActivityDetailsModalProps> = ({
 
     const showJoinButton = !isCreatorOfActivity;
 
-    
 
     return (
       <div className="modal">
@@ -35,7 +54,18 @@ const ActivityDetailsModal: React.FC<ActivityDetailsModalProps> = ({
           <p>Nombre: {activity.nameActivity}</p>
           <p>Fecha: {new Date(activity.dateActivity).toISOString().substr(0, 10)}</p>
           <p>Descripción: {activity.descriptionActivity}</p>
-          <p>Creador: {activity.creatorActivity}</p>
+          {creatorUser && (
+            <Link to={`/user/${creatorUser.uuid}`} className="user-link">
+              <div className="post__header">
+                <img className="post__profile-img" src={`${creatorUser.photoUser}`} alt="Profile"/>
+                  <div className="post__info">
+                    <p className="post__username_header">Creador: {creatorAppName}</p>
+                  </div>
+              </div>
+            </Link>
+            )
+          }
+
           <p>Participantes: {activity.participantsActivity?.join(", ")}</p>
           <button onClick={onClose}>Cerrar</button>
           {showJoinButton && (
--- lplan-web/src/pages/calendarevents.page/calendarevents.page.tsx
+++ lplan-web/src/pages/calendarevents.page/calendarevents.page.tsx
@@ -27,6 +27,9 @@ const CalendarEvents = () => {
   const [selectedTimetable, setSelectedTimetable] = useState("My Timetable");
   const [currentPage, setCurrentPage] = useState(1);
   const [recargar, setRecargar] = useState(false);
+  const [userList, setUserList] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -57,22 +60,27 @@ const CalendarEvents = () => {
           console.log("Feed de horarios de seguidores");
           console.log("Page", currentPage);
           const numPage = currentPage.toString();
-          const otherScheduleResponse = await ActivityService.getOtherSchedule(userId, numPage, date);
-          console.log(otherScheduleResponse);
-          console.log(otherScheduleResponse.data);
-          setListActivities(otherScheduleResponse.data.activities);
-          const response = await UserService.getPerson(otherScheduleResponse.data.uuid);
-          const user = response.data;
-          setOtherUser(user);
+          if (selectedUser && selectedUser.uuid) {
+            const selectedUserActivities = await ActivityService.getMySchedule(selectedUser.uuid, date);
+            setListActivities(selectedUserActivities.data);
+            setCurrentPage(1);
+          }else {
+            const otherScheduleResponse = await ActivityService.getOtherSchedule(userId, numPage, date);
+            console.log(otherScheduleResponse);
+            console.log(otherScheduleResponse.data);
+            setListActivities(otherScheduleResponse.data.activities);
+            const response = await UserService.getPerson(otherScheduleResponse.data.uuid);
+            const user = response.data;
+            setOtherUser(user);
+          }          
         }
       } catch (error) {
         navigate("*");
       }
     };
-  
     fetchData();
 
-  }, [selectedTimetable, currentPage, recargar]);
+  }, [selectedTimetable, currentPage, recargar, selectedUser]);
 
   const handleTimetableChange = (timetable: string) => {
     setSelectedTimetable(timetable);
@@ -82,16 +90,34 @@ const CalendarEvents = () => {
     setCurrentPage((prevPage) => prevPage + increment);
   };
 
+  const handleSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+  
+    if (query.length > 0) {
+      try {
+        const response = await UserService.searchUsers(query);
+        setUserList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      // Si la consulta está vacía, muestra todos los usuarios
+      setUserList([]);
+    }
+  };
+
   return (
     <div>
       <Navbar/>
       <div className="titleContainer_Calendar">
         <h1 className="titleSection">Calendar</h1>
+        
       </div>
+      
       <div className="contentContainer">
         <div className="schedule-buttons">
-          <button className={selectedTimetable === "My Timetable" ? "active" : ""} onClick={() => handleTimetableChange("My Timetable")}>Own</button>
-          <button className={selectedTimetable === "Timetable Feed" ? "active" : ""} onClick={() => handleTimetableChange("Timetable Feed")}>Friends</button>
+          <button className={selectedTimetable === "My Timetable" ? "active" : ""} onClick={() => {handleTimetableChange("My Timetable"); setSelectedUser(null);}}>Own</button>
+          <button className={selectedTimetable === "Timetable Feed" ? "active" : ""} onClick={() => {handleTimetableChange("Timetable Feed"); setSelectedUser(null);}}>Friends</button>
         </div>
         <div className="buttonsCalendar">
           {selectedTimetable === "My Timetable" &&(
@@ -100,10 +126,10 @@ const CalendarEvents = () => {
             </Link>
           )}
           {currentUser && currentUser.followedUser && selectedTimetable === "Timetable Feed" && currentPage > 1  &&(
-              <button className="nextPlanButton" onClick={() => handlePageChange(-1)}>Previous</button>
+              <button className="nextPlanButton" onClick={() => {handlePageChange(-1); setSelectedUser(null);} }>Previous</button>
           )}
           {currentUser && currentUser.followedUser && selectedTimetable === "Timetable Feed" && currentUser.followedUser?.length > currentPage &&(
-              <button className="nextPlanButton" onClick={() => handlePageChange(1)}>Next</button>
+              <button className="nextPlanButton" onClick={() => {handlePageChange(1); setSelectedUser(null);} }>Next</button>
           )}
         </div>
         {selectedTimetable === "My Timetable" && currentUser && (
@@ -123,21 +149,72 @@ const CalendarEvents = () => {
             </Link>
           </div>
         )}{selectedTimetable === "Timetable Feed" && otherUser && (
+          <><div className="SearchContainer">
+            <input
+              type="text"
+              placeholder="Search Users"
+              value={searchQuery}
+              onChange={(event) => {
+                setSearchQuery(event.target.value);
+                handleSearch(event);
+              }}
+            />
+            <div className="cardsUsers-Calendar">
+              {userList.length > 0 ? (
+                <ul>
+                  {userList.map((user: User) => (
+                    <li key={user.uuid}>
+                      {<button className="user-link-Calendar" onClick={() => setSelectedUser(user)}>
+                          <div className="user">
+                            {user.photoUser ? (<img src={user.photoUser} alt={user.nameUser} className="user__profile-img-Calendar" />) : (
+                              <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="profile-img-card-Calendar" />
+                            )}
+                            <div className="user__info-Calendar">
+                              <p className="user__name-Calendar">{user.nameUser} {user.surnameUser}</p>
+                              <p className="user__username-Calendar">@{user.appUser}</p>
+                            </div>
+                          </div>
+                        </button>}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <h1 className="usersnotfound">User Not Found</h1>
+              )}
+            </div>
+          </div>
+          
           <div className="userCalendarContainer">
-            <Link to={`/user/${otherUser?.uuid}`} className="userLink">
+            {selectedUser ? (
+              <Link to={`/user/${selectedUser?.uuid}`} className="userLink">
               <div className="user_calendar">
                 {otherUser.photoUser ? (
-                  <img src={otherUser.photoUser} alt={otherUser.nameUser} className="user_profile-img"/>
+                  <img src={selectedUser.photoUser} alt={selectedUser.nameUser} className="user_profile-img" />
                 ) : (
-                  <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="user_profile-img"/>
+                  <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="user_profile-img" />
                 )}
                 <div className="user__info">
-                  <p className="user__ns">{otherUser.nameUser} {otherUser.surnameUser}</p>
-                  <p className="user__un">@{otherUser.appUser}</p>
+                  <p className="user__ns">{selectedUser.nameUser} {selectedUser.surnameUser}</p>
+                  <p className="user__un">@{selectedUser.appUser}</p>
                 </div>
               </div>
             </Link>
-          </div>
+            ):(
+              <Link to={`/user/${otherUser?.uuid}`} className="userLink">
+                <div className="user_calendar">
+                  {otherUser.photoUser ? (
+                    <img src={otherUser.photoUser} alt={otherUser.nameUser} className="user_profile-img" />
+                  ) : (
+                    <img src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="profile-img" className="user_profile-img" />
+                  )}
+                  <div className="user__info">
+                    <p className="user__ns">{otherUser.nameUser} {otherUser.surnameUser}</p>
+                    <p className="user__un">@{otherUser.appUser}</p>
+                  </div>
+                </div>
+              </Link>
+            )}
+            </div></>
         )};
         <div className="calendar">
           <Calendar activities={listActivities} uuid={uuid} showWeekButton={false} showDayButton={false} showMonthButton={false} showWeekChangeButtons={true} editable={selectedTimetable === "My Timetable"} selectedTimetable={selectedTimetable} showAllDay={false} userId={currentUser?.uuid || ""} recargar={recargar} setRecargar={setRecargar}/>
